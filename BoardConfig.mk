27f9bbba1e2bb80358ac236046c2bda88558cfa4
mr-bad-guy <eyoelleykun00@gmail.com>
2026-01-21 19:33:14 +0300

    done

---
+++ BoardConfig.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2020-2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Inherit from the common tree
+
+include device/samsung/a13-common/BoardConfigCommon.mk
+
+## Inherit from the proprietary configuration
+
+include vendor/samsung/a13/BoardConfigVendor.mk
+
+DEVICE_PATH := device/samsung/a13
+
+## OTA TARGET
+TARGET_OTA_ASSERT_DEVICE := a13
+
+## Kernel
+TARGET_KERNEL_CONFIG := exynos850-a13xx_defconfig
+
+## Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
---
+++ lineage_a13.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2020-2025 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+## Inherit from a13 device
+$(call inherit-product, device/samsung/a13/device.mk)
+
+
+#fix
+
+## Boot Animation
+TARGET_SCREEN_HEIGHT := 2408
+TARGET_SCREEN_WIDTH := 1080
+
+## Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+#fix
+
+## Device identifier, this must come after all inclusions
+PRODUCT_DEVICE := a13
+PRODUCT_NAME := lineage_a13
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A135F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_SHIPPING_API_LEVEL := 34
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
